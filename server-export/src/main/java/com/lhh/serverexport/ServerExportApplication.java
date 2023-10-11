66b7cfdc399c8314244315b5513c788a9594ef29
lvhh <1260700062@qq.com>
2023-10-11 16:52:08 +0800

    export服务去掉无用代码

--- server-export/src/main/java/com/lhh/serverexport/ServerExportApplication.java
+++ server-export/src/main/java/com/lhh/serverexport/ServerExportApplication.java
@@ -3,13 +3,14 @@ package com.lhh.serverexport;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableRabbit
 @EnableEurekaClient
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class ServerExportApplication {
 
     public static void main(String[] args) {
